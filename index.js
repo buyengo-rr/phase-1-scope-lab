242083383307f56c7d7315cffddf2e2361ae1309
diagne <diagnebuyengo@gmail.com>
2025-03-29 18:49:58 +0300

    Scope

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "someone"; 
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; 
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "someone else";
+}
